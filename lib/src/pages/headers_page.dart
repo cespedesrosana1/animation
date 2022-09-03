974ce911d8c43b7deb0ef4491d75343303543446
S684256 <rosana.cespedes@evertecinc.com>
2022-09-03 11:12:40 -0300

    Animation Controller

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+import 'animaciones_page.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimacionesPage(),
+    );
+  }
+}
